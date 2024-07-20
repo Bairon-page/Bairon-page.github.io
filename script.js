9e5a3aa8c8abf77b01ae06685e4f0228924b9c53
Bairon-page <bairon9239@gmail.com>
2024-07-20 14:09:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let contenedor = document.getElementById("contenedor");
+
+
+
+
+
+
+function mt() {
+contenedor.innerHTML="esta fue una prueba para mis conocimentos de html css y javaScript";
+  
+
+
+  
+}
